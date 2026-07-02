c55ee6dc24736bc6466a6ca597db386f7685601a
Muzzamil Khan <muzzamil.akhan@gmail.com>
2026-07-03 09:56:51 +1000

    feat(holidays): re-key Holiday to User, drop canEditHolidays column

---
+++ prisma/migrations/20260703095602_user_level_holidays_drop_can_edit_holidays/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accountId` on the `Holiday` table. All the data in this column will be lost.
+  - You are about to drop the column `canEditHolidays` on the `AccountMembership` table. All the data in this column will be lost.
+  - You are about to drop the column `canEditHolidays` on the `ShareInvite` table. All the data in this column will be lost.
+  - A unique constraint covering the columns `[userId,name,source]` on the table `Holiday` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `userId` to the `Holiday` table without a default value. This is required to change the column type `public."Holiday"."accountId"` to `public."Holiday"."userId"`.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Holiday" DROP CONSTRAINT "Holiday_accountId_fkey";
+
+-- DropIndex
+DROP INDEX "Holiday_accountId_name_source_key";
+
+-- AlterTable
+ALTER TABLE "Holiday" DROP COLUMN "accountId",
+ADD COLUMN "userId" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "AccountMembership" DROP COLUMN "canEditHolidays";
+
+-- AlterTable
+ALTER TABLE "ShareInvite" DROP COLUMN "canEditHolidays";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Holiday_userId_name_source_key" ON "Holiday"("userId", "name", "source");
+
+-- AddForeignKey
+ALTER TABLE "Holiday" ADD CONSTRAINT "Holiday_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE;
